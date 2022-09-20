3c0ba1a86292d8ea2d75ce5655b6cabe1fbbad68
Juan Carlos <jrcarlossc@gmail.com>
2022-09-20 18:16:29 -0500

    Se mejoró la estructura para que sea responsive a cualquier dispositivo

---
+++ script.js
@@ -0,0 +1,8 @@
+let imgmuneco= document.getElementById("");
+
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keyup", e =>{
+  textarea.style.height = "63px";
+  let scHeight = e.target.scrollHeight;
+  textarea.style.height = `${scHeight}px`;
+});
